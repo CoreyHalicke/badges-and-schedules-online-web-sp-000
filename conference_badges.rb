eb658529d4b2741e5e0f2e5ee966bdd4921c91a1
Corey H <c.l.halicke@gmail.com>
2019-02-12 01:40:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -23,7 +23,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  badges = batch_badge_creator(attendees)
+  badges[] = batch_badge_creator(attendees)
   rooms = assign_rooms(attendees)
   
   
